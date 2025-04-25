ebefdb96f956a1ded6c7074bd23563d3a3a4aa38
Junseo <junseoadobe@gmail.com>
2025-04-25 14:21:42 +0900

    [Map]맵 생성 구현(2025.04.25)
    맵 생성 구현
    -맵 타일 이동
    -맵 풀링
    -맵 자동 생성

---
+++ Assets/Script/MapGenerator.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public Transform spawnPoint;
+    public MapTilePool tilePool;
+    public int tileCount = 7;
+    public float tileLength = 10f;
+
+    private Queue<GameObject> activeTiles = new Queue<GameObject>();
+
+    void Start()
+    {
+        // 타일들을 초기 위치부터 지정된 개수만큼 생성
+        for (int i = 0; i < tileCount; i++)
+        {
+            float zPos = spawnPoint.position.z + i * tileLength;
+            GameObject tile = tilePool.GetTile();
+            tile.transform.position = new Vector3(0, 0, zPos);
+            tile.GetComponent<MapMover>().SetGenerator(this);
+            activeTiles.Enqueue(tile);
+        }
+    }
+
+    public void RecycleTile(GameObject tile)
+    {
+        // 마지막 타일 위치 기준으로 새로운 위치 지정
+        GameObject lastTile = GetLastTile();
+        float newZ = lastTile.transform.position.z + tileLength;
+        tile.transform.position = new Vector3(0, 0, newZ);
+
+        activeTiles.Dequeue();
+        activeTiles.Enqueue(tile);
+    }
+
+    private GameObject GetLastTile()
+    {
+        GameObject[] activeArray = activeTiles.ToArray();
+        return activeArray[activeArray.Length - 1];
+    }
+}
